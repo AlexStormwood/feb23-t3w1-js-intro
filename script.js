fbde8a13301d4be96a9d28828a813926b663e237
Alex H <alex.holder@bigfootds.com>
2023-08-15 20:51:36 +1000

    feat: add basic JS to page

---
+++ script.js
@@ -0,0 +1,13 @@
+
+/**
+ * This is a function
+ * @param {*} someArgument - Some data given to the function
+ */
+function someFunction(someArgument) {
+	console.log("This was given to the function:" + someArgument);
+}
+
+
+someFunction([1, 2, "banana"]);
+
+
